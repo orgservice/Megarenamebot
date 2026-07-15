848608fea5dc6dea47310469d2a56485889525ee
Hantaiflixs <souravbosu017@gmail.com>
2026-07-16 00:02:51 +0600

    Add do_HEAD method to HealthHandler

--- bot.py
+++ bot.py
@@ -783,8 +783,14 @@ class HealthHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.end_headers()
         self.wfile.write(b"Bot is alive!")
+        
+    def do_HEAD(self):
+        self.send_response(200)
+        self.end_headers()
+
     def log_message(self, format, *args):
         pass
+
 
 def start_health_server():
     port = int(os.getenv("PORT", 8000))
